6bacfe38673073e244c2a72fd1b443c9e61bf2ca
Theo Puteaux <theo.ptx@laposte.net>
2022-08-10 21:57:09 +0200

    mise en plae de redux

--- src/index.jsx
+++ src/index.jsx
@@ -5,17 +5,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home.jsx'
 import SignIn from './pages/SignIn.jsx'
 import User from './pages/User.jsx'
+import { Provider } from "react-redux";
+import store from "./utils/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/login' element={<SignIn/>}></Route>
-        <Route path='/profile' element={<User/>}></Route>
-      </Routes>
-    </Router>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/login' element={<SignIn />}></Route>
+          <Route path='/profile' element={<User />}></Route>
+        </Routes>
+      </Router>
+    </React.StrictMode>
+  </Provider>
+
 )
