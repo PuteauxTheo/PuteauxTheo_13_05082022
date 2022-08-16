9997d97a9014ad6ee2ff2bb2b42098c69bb0bfff
Theo Puteaux <theo.ptx@laposte.net>
2022-08-16 12:39:54 +0200

    ajout du header sign out quand on est sur la page User

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,32 @@ import { Link } from 'react-router-dom'
 import argentBank from './../assets/argentBankLogo.png'
 
 export default function Header() {
+
+    
+    const token = localStorage.getItem('token') || sessionStorage.getItem('token')
+
+    function signOut(){
+        localStorage.clear()
+        sessionStorage.clear()        
+    }
+
     return (
+
+        token ? 
+        
+        <nav className="main-nav">
+            <Link className="main-nav-logo" to="/">
+                <img className="main-nav-logo-image" src={argentBank} alt="Argent Bank Logo"/>
+                <h1 className="sr-only">Argent Bank</h1>
+            </Link>
+            <div>
+                <Link className="main-nav-item" to="/" onClick={signOut}>
+                    <i className="fa fa-user-circle"></i>
+                    Sign out
+                </Link>
+            </div>
+        </nav>
+        :
         <nav className="main-nav">
             <Link className="main-nav-logo" to="/">
                 <img className="main-nav-logo-image" src={argentBank} alt="Argent Bank Logo"/>
@@ -15,5 +40,6 @@ export default function Header() {
                 </Link>
             </div>
         </nav>
+
     )
 }
