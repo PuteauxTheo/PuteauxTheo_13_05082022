56301ad6ec41b7ab1c0353202af281be0827404f
Theo Puteaux <theo.ptx@laposte.net>
2022-08-16 12:41:28 +0200

    utiliser des data en dynamique pour l'username

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,14 +1,21 @@
+import { useSelector } from "react-redux"
 import { Footer } from "../components/Footer"
 import Header from "../components/Header"
 import Transaction from "../components/Transaction"
+import { selectUser } from "../utils/selector"
 
 export default function User() {
+
+    const user = useSelector(selectUser)
+    console.log(user.data)
+
+    const userName = <span className="username">{user.data.firstName} {user.data.lastName}</span>
     return (
         <div>
             <Header />
             <main className="main">
                 <div className="header">
-                    <h1>Welcome back<br />Tony Jarvis!</h1>
+                    <h1>Welcome back<br />{userName}</h1>
                     <button className="edit-button">Edit Name</button>
                 </div>
                 <h2 className="sr-only">Accounts</h2>
