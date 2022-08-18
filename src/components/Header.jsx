fd4a616bc45ca44a285e79e41b5a88c88411b012
Theo Puteaux <theo.ptx@laposte.net>
2022-08-18 14:30:59 +0200

    ajout des informations dans la nav bar signout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,48 +1,54 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { actions } from '../features/user'
+import { selectUser } from '../utils/selector'
 import argentBank from './../assets/argentBankLogo.png'
 
 export default function Header() {
 
+    const user = useSelector(selectUser)
     const dispatch = useDispatch()
     const token = localStorage.getItem('token') || sessionStorage.getItem('token')
 
-    function signOut(){
+    function signOut() {
         localStorage.clear()
-        sessionStorage.clear()     
-        dispatch(actions.reset())   
+        sessionStorage.clear()
+        dispatch(actions.reset())
     }
 
     return (
 
-        token ? 
-        
-        <nav className="main-nav">
-            <Link className="main-nav-logo" to="/">
-                <img className="main-nav-logo-image" src={argentBank} alt="Argent Bank Logo"/>
-                <h1 className="sr-only">Argent Bank</h1>
-            </Link>
-            <div>
-                <Link className="main-nav-item" to="/" onClick={signOut}>
-                    <i className="fa fa-user-circle"></i>
-                    Sign out
+        token ?
+
+            <nav className="main-nav">
+                <Link className="main-nav-logo" to="/">
+                    <img className="main-nav-logo-image" src={argentBank} alt="Argent Bank Logo" />
+                    <h1 className="sr-only">Argent Bank</h1>
                 </Link>
-            </div>
-        </nav>
-        :
-        <nav className="main-nav">
-            <Link className="main-nav-logo" to="/">
-                <img className="main-nav-logo-image" src={argentBank} alt="Argent Bank Logo"/>
-                <h1 className="sr-only">Argent Bank</h1>
-            </Link>
-            <div>
-                <Link className="main-nav-item" to="/login">
-                    <i className="fa fa-user-circle"></i>
-                    Sign In
+                <div>
+                    <Link to='/profile' className='main-nav-item'>
+                        <i className="fa fa-user-circle"></i>
+                        {user.data.firstName}
+                    </Link>
+                    <Link className="main-nav-item" to="/" onClick={signOut}>
+                        <i className="fa fa-sign-out"></i>
+                        Sign out
+                    </Link>
+                </div>
+            </nav>
+            :
+            <nav className="main-nav">
+                <Link className="main-nav-logo" to="/">
+                    <img className="main-nav-logo-image" src={argentBank} alt="Argent Bank Logo" />
+                    <h1 className="sr-only">Argent Bank</h1>
                 </Link>
-            </div>
-        </nav>
+                <div>
+                    <Link className="main-nav-item" to="/login">
+                        <i className="fa fa-user-circle"></i>
+                        Sign In
+                    </Link>
+                </div>
+            </nav>
 
     )
 }
